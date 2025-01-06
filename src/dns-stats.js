c90b01be49b328b8d19eff6aabc465b3d90c54fe
amiryusupov <amiryusupov.070@gmail.com>
2025-01-07 02:14:40 +0500

    fix: 3 easy tasks finished

--- src/dns-stats.js
+++ src/dns-stats.js
@@ -22,9 +22,21 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getDNSStats(domains) {
+  let result = {};
+  for (let i = 0; i < domains.length; i++) {
+    let domain = domains[i].split('.').reverse();
+    let key = '';
+    for (let j = 0; j < domain.length; j++) {
+      key += `.${domain[j]}`;
+      if (result[key]) {
+        result[key]++;
+      } else {
+        result[key] = 1;
+      }
+    }
+  }
+  return result;
 }
 
 module.exports = {
